edc835cb785e973e1001682fe84a5c69baf12288
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-06-12 10:44:40 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function EyesOnMe() {
+    return(
+        <div>
+            <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
